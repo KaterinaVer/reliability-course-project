115f4276e5d04aab6456720812df333c27815f23
KaterinaVer <katyaveremchuk7@gmail.com>
2020-05-13 23:44:38 +0300

    add frontend

--- src/main/java/com/course/reliability/rest/CustomerInformationController.java
+++ src/main/java/com/course/reliability/rest/CustomerInformationController.java
@@ -4,42 +4,53 @@ import com.course.reliability.model.CustomerInformation;
 import com.course.reliability.service.CustomerInformationService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.ws.rs.Path;
+import java.util.List;
 import java.util.Optional;
 
-@RestController
-@Path("/customer-information")
+@Controller
+//@RequestMapping("/customer-information")
 @AllArgsConstructor
 public class CustomerInformationController {
     private final CustomerInformationService customerInformationService;
 
-    @GetMapping("/")
-    public Iterable<CustomerInformation> getAllCustomers() {
-        return customerInformationService.getCustomers();
+    @RequestMapping
+    public String getAllCustomers(final Model model) {
+        Iterable<CustomerInformation> customers = customerInformationService.getCustomers();
+
+        model.addAttribute("customers", customers);
+        return "list-customers";
     }
 
-    @GetMapping("/{id}")
+    @RequestMapping("/{id}")
     public Optional<CustomerInformation> getCustomerInformation(@PathVariable("id") Integer id) {
         return customerInformationService.getCustomerInformationById(id);
     }
 
-    @PostMapping("/")
-    public ResponseEntity<CustomerInformation> createCustomerInformation(@RequestBody CustomerInformation customerInformation) {
-        CustomerInformation savedCustomerInformation = customerInformationService.addCustomerInformation(customerInformation);
-        return ResponseEntity.ok(savedCustomerInformation);
-    }
-
-    @PutMapping("/{id}")
-    public void updateCustomerInformation(@PathVariable("id") Integer id, @Valid @RequestBody  CustomerInformation customerInformation) {
-        customerInformation.setId(id);
-
+    @RequestMapping(path = "/add", method = RequestMethod.POST)
+    public String createCustomerInformation(CustomerInformation customerInformation) {
         customerInformationService.addCustomerInformation(customerInformation);
+        return "redirect:/";
     }
 
-    @DeleteMapping("/{id}")
+    @RequestMapping(path = {"/edit", "/edit/{id}"})
+    public String updateCustomerInformation(Model model, @PathVariable("id") Optional<Integer> id) {
+        if (id.isPresent()) {
+            Optional<CustomerInformation> customerInformation = customerInformationService.getCustomerInformationById(id.get());
+            model.addAttribute("customer", customerInformation);
+        } else {
+            model.addAttribute("customer", new CustomerInformation());
+        }
+
+        return "add-edit-customer";
+    }
+
+    @RequestMapping("/delete/{id}")
     public void deleteCustomerInformation(@PathVariable("id") Integer id) {
         customerInformationService.deleteCustomerInformation(id);
     }
